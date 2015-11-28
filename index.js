1a1cbb0189d4bb98f2f62377fe12b9836fa0bb20
Eivind Ingebrigtsen <o95@storebrand.no>
2015-11-28 16:48:29 +0100

    fixing embarrasing mistake

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser');
 
 
 // configuring middleware for express 
